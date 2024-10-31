cc3bd7a408457530bc828624923c5ae88abbce91
Muhammad Habib <muhammadhabib@techmatetech.com>
2024-10-31 14:09:58 +0500

    setup email templating

--- apps/cx-api/src/default.controller.ts
+++ apps/cx-api/src/default.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MailClientService } from './mail-client/mail-client.service';
 import { ApiBody, ApiProperty } from '@nestjs/swagger';
+import { compileTemplate } from '@app/shared-lib/mail-template-utility';
 
 export class TestMailDto {
   @ApiProperty({
@@ -45,6 +46,28 @@ export class DefaultController {
     try {
       const { to, subject, text, html } = body;
       await this.mailClientService.sendMail(to, subject, text, html);
+      return { message: 'Email sent successfully!' };
+    } catch (error) {
+      console.log('Mail Error ==>', error);
+      return { message: 'Email sending failed' };
+    }
+  }
+
+  @Post('/send-mail-with-template')
+  @ApiBody({
+    description: 'Data for sending an email',
+    type: TestMailDto,
+  })
+  async sendEmailWithTemplate(
+    @Body() body: { to: string; subject: string; text: string; html: string },
+  ) {
+    try {
+      const { to, subject, text } = body;
+      const compiledHtml = compileTemplate('send-mail', {
+        email: to,
+      });
+      console.log('Compiled Html', compiledHtml);
+      await this.mailClientService.sendMail(to, subject, text, compiledHtml);
       return { message: 'Email sent successfully!' };
     } catch (error) {
       console.log('Mail Error ==>', error);
